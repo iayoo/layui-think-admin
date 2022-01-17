83044c7a73ecbd45b0b8f20c069556425aa1088e
vilnius <vilnius94@163.com>
2022-01-18 01:35:10 +0800

    # tab 删除控制修改

--- public/static/admin/admin.js
+++ public/static/admin/admin.js
@@ -9,18 +9,18 @@ layui.config({
 
 layui.use(['element','dropdown','jquery'],function (){
     let element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
-    let dropdown = layui.dropdown;
+    // let dropdown = layui.dropdown;
     let $ = layui.jquery;
 
     let tabs = [];
 
 
-    //菜单点击事件，其中 docDemoMenu1 对应的是菜单结构上的 id 指
-    dropdown.on('click(docSideMenu)', function(options){
-        // let othis = $(this); //当前菜单列表的 DOM 对象
-        console.log(111)
-        // console.log(options); //菜单列表的 lay-options 中的参数
-    });
+    // //菜单点击事件，其中 docDemoMenu1 对应的是菜单结构上的 id 指
+    // dropdown.on('click(docSideMenu)', function(options){
+    //     // let othis = $(this); //当前菜单列表的 DOM 对象
+    //     console.log(111)
+    //     // console.log(options); //菜单列表的 lay-options 中的参数
+    // });
 
     function handleTagChange(id,href,title){
         let isHasTab = false;
@@ -32,7 +32,7 @@ layui.use(['element','dropdown','jquery'],function (){
         })
         if (href !== undefined && !isHasTab){
             //新增一个Tab项
-            element.tabAdd('window-tab', {
+            let res = element.tabAdd('window-tab', {
                 title: title//用于演示
                 ,content:  '<iframe id="' + id + '" data-frameid="' + id + '" scrolling="auto" frameborder="0" src="' +
                     href + '" style="width:100%;height:100%;"></iframe>'
@@ -42,6 +42,10 @@ layui.use(['element','dropdown','jquery'],function (){
             element.tabChange('window-tab', id); //切换到：用户管理
         }
     }
+
+    element.on('tabDelete(window-tab)', function(data){
+        tabs.splice(data.index,1)
+    });
 
     //监听导航点击
     element.on('nav(side_menu)', function(elem){
