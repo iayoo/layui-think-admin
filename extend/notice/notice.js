cd820610a6e66f0ccc24078d04b8fb2762bb6b7d
Iayoo <vilnius94@163.com>
2022-01-17 17:06:30 +0800

    # 清除toast功能

--- extend/notice/notice.js
+++ extend/notice/notice.js
@@ -6,7 +6,9 @@
 
 
             let notice = {
-                // clear: clear,
+                clear: clear,
+                stack:[],
+                index:0,
                 // remove: remove,
                 // error: error,
                 // getContainer: getContainer,
@@ -19,6 +21,23 @@
 
             };
 
+            function clear(index){
+                if (index !== undefined){
+                    $("#ia-notification_"+index, top.document).fadeOut();
+                    if (notice.stack[index] !== undefined){
+                        notice.stack[index] = undefined;
+                    }
+                    return true;
+                }
+                let elmList = $(".ia-notification_list > .ia-notification", top.document);
+                for (let i = 0;i<elmList.length;i++){
+                    $(elmList[i]).fadeOut();
+                    setTimeout(function (){
+                        $(elmList[i]).remove();
+                    },500)
+                }
+            }
+
             notice.toast = function (message,times){
                 // console.log(2222)
                 // console.log($("body", top.document).html())
@@ -28,8 +47,9 @@
             }
 
             function show(){
+                notice.index+=1
                 // <div class="t-notification__icon"><svg fill="none" viewBox="0 0 16 16" width="1em" height="1em" class="t-icon t-icon-info-circle-filled t-is-warning"><path fill="currentColor" d="M8 15A7 7 0 108 1a7 7 0 000 14zM7.4 4h1.2v1.2H7.4V4zm.1 2.5h1V12h-1V6.5z" fill-opacity="0.9"></path></svg></div>
-                let html = "<div class='ia-notification'>" +
+                let html = "<div class='ia-notification' id='ia-notification_" + notice.index + "'>" +
                     '<div class="icon"><svg fill="none" viewBox="0 0 16 16" width="1em" height="1em" class="t-icon t-icon-info-circle-filled t-is-warning"><path fill="currentColor" d="M8 15A7 7 0 108 1a7 7 0 000 14zM7.4 4h1.2v1.2H7.4V4zm.1 2.5h1V12h-1V6.5z" fill-opacity="0.9"></path></svg></div>' +
                     '<div class="main"><div class="title_contain"><span class="title">告警通知</span><span class="message_close"></span></div><div class="content">这是一条告警的消息通知</div></div>' +
                     "</div>";
@@ -46,9 +66,9 @@
                 elm.fadeTo(500, 1, function() {
                     console.log('完成');
                 })
-                // let out = setTimeout (function () {
-                //     elm.fadeOut(500)
-                // }, notice.options.times)
+                let out = setTimeout (function () {
+                    elm.fadeOut(10500)
+                }, notice.options.times)
 
         }
 
