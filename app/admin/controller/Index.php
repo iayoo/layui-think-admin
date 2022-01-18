09b840d00444bbf1ab8b953404d404e91f918cc8
Iayoo <vilnius94@163.com>
2022-01-18 13:45:33 +0800

    # 首页样式

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -20,66 +20,10 @@ class Index extends BaseController
     /**
      * 显示创建资源表单页.
      *
-     * @return \think\Response
      */
-    public function create()
+    public function home()
     {
         //
-    }
-
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request  $request
-     * @return \think\Response
-     */
-    public function save(Request $request)
-    {
-        //
-    }
-
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function read($id)
-    {
-        //
-    }
-
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * 删除指定资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function delete($id)
-    {
-        //
+        return $this->fetch();
     }
 }
