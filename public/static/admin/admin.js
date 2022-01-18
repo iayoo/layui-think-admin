5de828efa606f2d5a29f1de0c4d01e3c05201298
Iayoo <vilnius94@163.com>
2022-01-18 17:15:08 +0800

    # 新增刷新功能

--- public/static/admin/admin.js
+++ public/static/admin/admin.js
@@ -1,21 +1,32 @@
 layui.extend({
-    setting: 'config' //配置模块
-    ,
-    admin: 'lib/admin' //核心模块
-    ,
-    view: 'lib/view' //视图渲染模块
+    setting: 'config'
 }).define(['setting','element','dropdown','jquery'], function(exports) {
-    console.log(22221111)
     let element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
     // let dropdown = layui.dropdown;
     let $ = layui.jquery;
     let setting = layui.setting;
 
-    let tabs = [];
+    let win = $('body');
+
+    let tabs = [],tabsIndex = 0;
+
+
 
     let admin = {
         open:openPage
     };
+
+    let adminEvents = {
+        /**
+         * 刷新
+         */
+        refresh:function () {
+            let curIframe = $(".layui-tab-content .layui-tab-item").eq(tabsIndex).find("iframe")[0];
+            // console.log('刷新页面:index-'+tabsIndex)
+            // console.log(curIframe)
+            curIframe.contentWindow.location.reload(true);
+        }
+    }
 
     function openPage(url,title,id){
         handleTagChange(id,url,title)
@@ -38,9 +49,9 @@ layui.extend({
         })
         if (href !== undefined && !isHasTab){
             //新增一个Tab项
-            let res = element.tabAdd('window-tab', {
+            element.tabAdd('window-tab', {
                 title: title//用于演示
-                ,content:  '<iframe id="' + id + '" data-frameid="' + id + '" scrolling="auto" frameborder="0" src="' +
+                ,content:  '<iframe id="' + id + '" data-frameid="' + id + '" frameborder="0" scrolling="auto" src="' +
                     href + '" style="width:100%;height:100%;"></iframe>'
                 ,id: id //实际使用一般是规定好的id，这里以时间戳模拟下
             })
@@ -53,12 +64,30 @@ layui.extend({
         tabs.splice(data.index,1)
     });
 
+    win.on('click', '*[layer-event]', function(){
+        let _this = $(this)
+            ,attrEvent = _this.attr('layer-event');
+        if (adminEvents[attrEvent]){
+            adminEvents[attrEvent].call(this, _this);
+        }
+    })
+
     //监听导航点击
     element.on('nav(side_menu)', function(elem){
         let href = $(elem).data('href');
         let hrefId = $(elem).attr('lay-id');
         handleTagChange(hrefId,href,elem.text())
     });
+
+    //监听tab点击
+    element.on('tab(window-tab)', function(data){
+        tabsIndex = data.index
+    });
+
+    //监听导航点击
+    element.on('nav(header_menu)', function(elem){
+    });
+
     // <i class="layui-icon layui-icon-home"></i>
     exports('admin',admin);
 });
